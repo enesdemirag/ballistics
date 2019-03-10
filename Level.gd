ab3f7d10f49826ed37e8b2d87ad0d087bab116a4
Enes Demirağ <demirag16@itu.edu.tr>
2019-03-10 17:59:43 +0000

    update level

--- Level.gd
+++ Level.gd
@@ -3,17 +3,15 @@ export (PackedScene) var Ball
 
 export (int) var ball_count
 var screensize
-var initial_cannon_rotation
 
 func _ready():
 	randomize()
-	print("Hello")
 	$Music.play()
 	screensize = get_viewport_rect().size
 	$CannonPath/CannonSpawnLocation.set_offset(randi())
 	$Cannon.position = $CannonPath/CannonSpawnLocation.position
-	initial_cannon_rotation = $CannonPath/CannonSpawnLocation.rotation + PI / 2
-	$Cannon.rotation = initial_cannon_rotation + rand_range(-PI / 2.5, PI / 2.5)
+	$Cannon.rotation = $CannonPath/CannonSpawnLocation.rotation + rand_range(PI / 10, 9 * PI / 10)
+	print("Hello World")
 
 func updateCannonRotation(rotation):
 	$Cannon.rotation += rotation * PI / 180
@@ -25,23 +23,18 @@ func _process(delta):
 		$Cannon.rotation += PI / 90
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_select"):
-		$BallTimer.start()
+	if Input.is_action_just_released("ui_select"):
+		if ball_count > 0:
+			var ball = Ball.instance()
+			add_child(ball)
+			ball.position = $Cannon.position
+			ball.rotation = $Cannon.rotation
+			ball.set_linear_velocity(Vector2(400, 0).rotated($Cannon.rotation - PI))
+			ball_count -= 1
 	if Input.is_action_pressed("ui_right"):
 		updateCannonRotation(1)
 	if Input.is_action_pressed("ui_left"):
 		updateCannonRotation(-1)
-
-func _on_BallTimer_timeout():
-	if ball_count > 0:
-		var ball = Ball.instance()
-		add_child(ball)
-		ball.position = $Cannon.position
-		ball.rotation = $Cannon.rotation
-		ball.set_linear_velocity(Vector2(400, 0).rotated($Cannon.rotation - PI))
-		ball_count -= 1
-	else:
-		pass
 
 func _on_Target_hit():
 	pass # Success
